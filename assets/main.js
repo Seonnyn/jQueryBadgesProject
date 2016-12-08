4be958e21822393fff9c6bb1cdc7c12ad0c911bc
Seonnyn <h.roeszler@gmail.com>
2016-12-08 15:04:39 +0000

    First Commit

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,16 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/Seonnyn.json',
+    type: 'GET',
+    datatype: 'jsonp',
+    success: function(response) {
+      console.log(response);
+      $.each(response.courses.completed, function(ind, elem) {
+        console.log(elem);
+        $('#badges').append('<div class="course"><h3>'+elem.title+'</h3><img src="'+elem.badge+'" /><a href="'+elem.url+'" target="_blank" class="btn btn-primary">See Course</a></div>');
+      });
+    },
+  });
 
 });
